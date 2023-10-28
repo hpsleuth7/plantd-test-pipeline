b22bebce5959d020d02deaf34688b730d5f617d1
murph <murphy.c.austin@gmail.com>
2023-10-28 15:02:05 -0400

    add log statements and correct for loop math

--- phase1.py
+++ phase1.py
@@ -84,11 +84,12 @@ async def upload(file: UploadFile = File(...)):
 			# get current time in seconds
 			skip_time = 2 # number of seconds in skip interval
 			if (int(time.time()) // skip_time) % 2 == 0:
+				print("Skipping time")
 				return {"status": "Success: time skip.", "code": 201}
-			 
+			print("Not skipping time") 
 			# do dummy work
 			for i in range(1000000):
-				for j in range(i):
+				for j in range(2, i):
 					if i % j == 0:
 						break
 
